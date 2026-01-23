ff1a54f2386b3639366ddae466545d076486816c
Parag Kumar <paragpk2005@gmail.com>
2026-01-23 23:19:07 +0530

    Neetcode 250 Day 3

---
+++ Neetcode 250/Two Pointers/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int i=0, j=1,k=1;
+        while(j<n){
+            if(nums[i]==nums[j]) j++;
+            else{
+                nums[i+1]=nums[j];
+                i++; j++; k++;
+            }
+        }
+        return k;
+    }
+}
