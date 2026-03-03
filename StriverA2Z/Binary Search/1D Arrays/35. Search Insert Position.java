2fab677eccb46890a7a13bbe27cd519e09d29b7c
Parag Kumar <paragpk2005@gmail.com>
2026-03-03 11:06:23 +0530

    Create 35. Search Insert Position.java

---
+++ StriverA2Z/Binary Search/1D Arrays/35. Search Insert Position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+        while(low<=high){
+            int middle=low+(high-low)/2;
+            if(nums[middle]==target) return middle;
+            else if(nums[middle]<target){
+                low=middle+1;
+            }
+            else{
+                high=middle-1;
+            }
+        }
+        return low;
+    }
+}
