e619c9367c98fba3888d1ec5a2bd435b506fdaac
Parag Kumar <paragpk2005@gmail.com>
2026-03-03 12:10:45 +0530

    Create 74. Search a 2D Matrix.java

---
+++ StriverA2Z/Binary Search/2d Arrays/74. Search a 2D Matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length; //rows
+        int n=matrix[0].length; //columns
+        int low=0, high=n*m-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid/n][mid%n]==target){
+                return true;
+            }
+            else if (matrix[mid/n][mid%n]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
