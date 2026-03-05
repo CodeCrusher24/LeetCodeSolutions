dc3f91bb1f1aa6a6f1a9c75784a180d489b83121
Parag Kumar <paragpk2005@gmail.com>
2026-03-05 19:41:51 +0530

    Neetcode 250 Day 9

---
+++ Neetcode 250/Arrays and Hashing/169. Majority Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int count=0;
+       int element=-1;
+       for(int num: nums){
+        if(count==0){
+            count=1;
+            element=num;
+        }
+        else if(element==num) count++;
+        else count--;
+       }
+       return element;
+        
+    }
+}
